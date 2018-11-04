5041b977d99bf382a63d22b4e7bce39916036e28
Ross Jennings <rossjjennings@gmail.com>
2018-11-03 22:55:27 -0400

    Add spectrum plot

---
+++ xpsrfits/plots.py
@@ -0,0 +1,13 @@
+import numpy as np
+import xarray as xr
+import matplotlib.pyplot as plt
+
+def spec_plot(ds, arr, colorbar=False):
+    fig, ax = plt.subplots()
+    arr_shifted = np.roll(arr, len(ds.phase)//2, axis=-1)
+    phase_shifted = ds.phase - ds.phase[len(ds.phase)//2]
+    pc = ax.pcolormesh(phase_shifted, ds.freq, arr_shifted)
+    if colorbar: plt.colorbar(pc)
+    ax.set(xlabel='Phase (ms)', ylabel='Frequency (MHz)')
+    
+    return ax
