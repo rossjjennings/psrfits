e37dd96be7ec522f062f60558d9324657d46e497
Ross Jennings <rossjjennings@gmail.com>
2018-11-19 13:58:27 -0500

    Add colormap option

--- xpsrfits/plots.py
+++ xpsrfits/plots.py
@@ -2,11 +2,11 @@ import numpy as np
 import xarray as xr
 import matplotlib.pyplot as plt
 
-def spec_plot(ds, arr, colorbar=False):
+def spec_plot(ds, arr, cmap='viridis', colorbar=False):
     fig, ax = plt.subplots()
     arr_shifted = np.roll(arr, len(ds.phase)//2, axis=-1)
     phase_shifted = ds.phase - ds.phase[len(ds.phase)//2]
-    pc = ax.pcolormesh(phase_shifted, ds.freq, arr_shifted)
+    pc = ax.pcolormesh(phase_shifted, ds.freq, arr_shifted, cmap=cmap)
     if colorbar: plt.colorbar(pc)
     ax.set(xlabel='Phase (ms)', ylabel='Frequency (MHz)')
     
