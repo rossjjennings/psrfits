9fc806f4f402224d4b1babc842038ac629d775d1
Ross Jennings <rossjjennings@gmail.com>
2021-01-30 16:58:00 -0600

    Rename datasets -> loading

--- xpsrfits/__init__.py
+++ xpsrfits/__init__.py
@@ -1,4 +1,4 @@
-from xpsrfits.datasets import read, load, ingest, get_pint_model
+from xpsrfits.loading import read, load, ingest, get_pint_model
 from xpsrfits.dispersion import dedisperse
 from xpsrfits.polarization import pscrunch, to_stokes
 from xpsrfits.baseline import remove_baseline
