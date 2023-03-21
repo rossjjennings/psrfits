e08d35f6d7bc83fd93fbb4424fe0f96daeccbc79
Ross Jennings <rossjjennings@gmail.com>
2023-03-21 14:11:32 -0400

    Clean up loading options

--- psrfits/loading.py
+++ psrfits/loading.py
@@ -7,7 +7,7 @@ from psrfits.dispersion import dedisperse, align_with_predictor
 from psrfits.baseline import remove_baseline
 
 def load(filename, weight=False, uniformize_freqs=False, prepare=False,
-         use_predictor=True, baseline_method='avgprof', output_polns=None):
+         use_predictor=True, baseline_method='avgprof', loader='lazy'):
     '''
     Open a PSRFITS file and load the contents into a Dataset.
 
@@ -17,9 +17,9 @@ def load(filename, weight=False, uniformize_freqs=False, prepare=False,
         the file. Typically these are proportional to the time-bandwidth product.
     uniformize_freqs (default: False): Whether to replace the frequencies present in
         the file by an educated guess at a uniformly-spaced version.
-    prepare (default: False): Whether to automatically dedisperse the data and
-        subtract the baseline from each profile. This can also be done after loading
-        using dedicated functions.
+    prepare (default: False): Whether to automatically dedisperse the data,
+        subtract the baseline from each profile, and convert coherence data to Stokes.
+        This can also be done after loading using dedicated functions.
     use_predictor (default: True): Whether to dedisperse using the Tempo2 predictor,
         if available, rather than the DM in the header. Ignored if `prepare` is `False`.
     baseline_method (default: 'avgprof'): The method used to determine the baseline
@@ -27,23 +27,21 @@ def load(filename, weight=False, uniformize_freqs=False, prepare=False,
         See psrfits.baseline.remove_baseline() for details.
     wcfreq (default: False): Whether to use a "weighted" center frequency. This can be
         used to replicate the behaviour of PyPulse, but generally is best left alone.
-    output_polns (default: None): Output polarizations to produce. The value 'IQUV'
-        will produce all four Stokes parameters; a value of 'I' gives intensity only.
-        Any other value will leave the polarizations in the file unchanged.
+    loader (default: 'lazy'): Possible values are 'lazy', 'memmap', or 'eager'. For 'lazy',
+        the data will be read into a Dask array using dask.delayed, and read only when
+        necessary. No file handle will be retained. For 'memmap', the data will be
+        memory-mapped and read into a Dask array, retaining a file handle. For 'eager',
+        the data will be read into memory immediately.
     '''
-    ds = DataFile.from_file(filename, uniformize_freqs=uniformize_freqs)
+    ds = DataFile.from_file(filename, uniformize_freqs=uniformize_freqs, loader=loader)
     if prepare:
         if use_predictor:
             try:
                 ds.align_with_predictor()
             except ValueError:
-                warnings.warn('No Tempo2 predictor present! Using fallback dedispersion method')
                 ds.dedisperse()
         else:
             ds.dedisperse()
         ds.remove_baseline()
-    if output_polns == 'I':
-        ds.pscrunch()
-    elif output_polns == 'IQUV':
         ds.to_stokes()
     return ds
