1caadb813f7556741aaf2e53f2d835474b30e5a1
Ross Jennings <rossjjennings@gmail.com>
2023-09-15 18:06:37 -0400

    Use 'memmap' loader by default in load()

--- psrfits/loading.py
+++ psrfits/loading.py
@@ -7,7 +7,7 @@ from psrfits.dispersion import dedisperse, align_with_predictor
 from psrfits.baseline import remove_baseline
 
 def load(filename, weight=False, uniformize_freqs=False, prepare=False,
-         use_predictor=True, baseline_method='avgprof', loader='lazy',
+         use_predictor=True, baseline_method='avgprof', loader='memmap',
          extrap=False):
     '''
     Open a PSRFITS file and load the contents into a Dataset.
@@ -26,11 +26,16 @@ def load(filename, weight=False, uniformize_freqs=False, prepare=False,
     baseline_method (default: 'avgprof'): The method used to determine the baseline
         level when it is to be removed. Options are 'avgprof', 'offpulse', and 'median'.
         See psrfits.baseline.remove_baseline() for details.
-    loader (default: 'lazy'): Possible values are 'lazy', 'memmap', or 'eager'. For 'lazy',
-        the data will be read into a Dask array using dask.delayed, and read only when
-        necessary. No file handle will be retained. For 'memmap', the data will be
-        memory-mapped and read into a Dask array, retaining a file handle. For 'eager',
-        the data will be read into memory immediately.
+    loader (default: 'memmap'):
+        With 'copy', the data will be read into memory immediately.
+        With 'memmap' (the default), the data will be memory mapped and read into
+            a Dask array, retaining a file handle.
+        With 'delayed', the data will be read into a Dask array using dask.delayed.
+            This is useful because it does not leave a file handle open, but is slower
+            than the 'memmap' approach when the data are first read.
+        With 'delayed_memmap', the data will be memory mapped and read into a Dask
+            array using dask.delayed, which should not leave a file handle open.
+        With 'astropy', the memory mapping internal to astropy.io.fits will be used.
     extrap (default: False): If `True`, allow extrapolation when calculating the pulse
         phase using a Tempo2 predictor. Ignored if `prepare` is `False`, or if no Tempo2
         predictor is present.
